d87097a1b3ae97b0be0c3d287a58e72663745296
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-03-13 21:00:38 -0600

    Add all assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-import Header from "./Components/header";
+import Nav from "./Components/Nav";
+import './styles/App.scss'
 
 class App extends Component {
   render() {
-    return <div className="Test">Hello World</div>
+    return (
+     <div className="Original">
+        <Nav></Nav>
+        Hello World
+      </div>
+    )
   }
 }
 
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <div className="sticky">
+            <div>
+                <a href="#home">
+                    <img></img>
+                </a>
+            </div>
+        </div>
+        )
+}
+
+export default Nav;
