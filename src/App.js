f7be0abf807e3ee3a3a9db5e569290b9e5ccbcac
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-03-13 23:58:02 -0600

    Add Intro component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import Nav from "./Components/Nav";
 import './styles/App.scss'
+import Nav from "./Components/Nav";
+import Introduction from "./Components/Intro";
 
 class App extends Component {
   render() {
     return (
      <div className="Original">
         <Nav></Nav>
+        <Introduction></Introduction>
       </div>
     )
   }
---
+++ src/Components/Intro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/Intro.scss'
+
+function Introduction() {
+    return (
+        <div className="Intro" id="home">
+            <div className="Intro-Text">
+                <h1 className="appear-first">Abdul-Rahime</h1>
+                <h1 className="appear-second">Kadri</h1>
+                <h1 className="appear-third">Web Developer</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Introduction;
