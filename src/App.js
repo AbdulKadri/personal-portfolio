9e3b8d76c5a145a4669bca759070052c646a757a
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-03-13 22:20:01 -0600

    Add Nav bar to homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
     return (
      <div className="Original">
         <Nav></Nav>
-        Hello World
       </div>
     )
   }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import logo from '../Assets/personal-logo.svg'
 
-const Nav = () => {
+function Nav() {
     return (
-        <div className="sticky">
-            <div>
+        <div className="nav sticky">
+            <div className="logo">
                 <a href="#home">
-                    <img></img>
+                    <img src={logo} alt="Personal logo"/>
                 </a>
             </div>
+
+            <div className="menu-items">
+                <ul>
+                    <li><a href="#home"><u>Home</u></a></li>
+                    <li><a href="#about"><u>About</u></a></li>
+                    <li><a href="#projects"><u>Projects</u></a></li>
+                    <li><a href="#contact"><u>Contact</u></a></li>
+                </ul>
+            </div>
         </div>
-        )
+    )
 }
 
 export default Nav;
