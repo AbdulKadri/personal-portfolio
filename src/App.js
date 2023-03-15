94cf62badeaf1687638a631c6eb638af7a52ecfa
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-03-14 22:01:02 -0600

    Add Footer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Introduction from "./Components/Intro";
 import About from "./Components/About"
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact"
+import Footer from "./Components/Footer";
 
 class App extends Component {
   render() {
     return (
-     <div className="Original">
+     <div>
         <Nav />
         <Introduction />
         <About />
         <Projects />
         <Contact />
+        <Footer />
       </div>
     )
   }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/Footer.scss'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>&copy; Copyright 2023, Abdul-Rahime Kadri</p>
+        </div>
+    )
+}
+
+export default Footer
