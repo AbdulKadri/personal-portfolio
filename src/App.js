9e48d2e78b01f441519dfc497aa78bdf177d2bd0
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-03-14 21:04:49 -0600

    Add projects layout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './styles/App.scss'
 import Nav from "./Components/Nav";
 import Introduction from "./Components/Intro";
 import About from './Components/About'
+import Projects from "./Components/Projects";
 
 class App extends Component {
   render() {
     return (
      <div className="Original">
-        <Nav></Nav>
-        <Introduction></Introduction>
-        <About></About>
+        <Nav />
+        <Introduction />
+        <About />
+        <Projects />
       </div>
     )
   }
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import '../styles/Projects.scss'
+
+const Projects = () => {
+    return (
+        <div className="project-container" id="projects">
+            <h2> <u>Projects</u> </h2>
+
+            <div className="project">
+                <div className="project-text">
+                    <h3>Project 1</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iste asperiores ut dolorum obcaecati vero ullam praesentium dolore corrupti nulla? Odio voluptates harum quasi in? Neque, quod? Aut, adipisci amet!</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat earum nobis cupiditate non officiis consequatur ea cum accusamus sed. Reprehenderit corrupti vitae minima enim quo. Nihil fugit excepturi nemo praesentium.</p>
+
+                    <br />
+
+                    <p>Created with Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+
+                    <br />
+                    <br />
+
+                    <p>Link to site</p>
+                    <p>Link to repo</p>
+                </div>
+
+                <div className="project-preview"></div>
+            </div>
+
+            <div className="project">
+                <div className="project-preview"> </div>
+
+                <div className="project-text">
+                    <h3>Project 2</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iste asperiores ut dolorum obcaecati vero ullam praesentium dolore corrupti nulla? Odio voluptates harum quasi in? Neque, quod? Aut, adipisci amet!</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat earum nobis cupiditate non officiis consequatur ea cum accusamus sed. Reprehenderit corrupti vitae minima enim quo. Nihil fugit excepturi nemo praesentium.</p>
+
+                    <br />
+
+                    <p>Created with Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+
+                    <br />
+                    <br />
+
+                    <p>Link to site</p>
+                    <p>Link to repo</p>
+                </div>
+            </div>
+
+            <div className="project">
+                <div className="project-text">
+                    <h3>Project 3</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iste asperiores ut dolorum obcaecati vero ullam praesentium dolore corrupti nulla? Odio voluptates harum quasi in? Neque, quod? Aut, adipisci amet!</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat earum nobis cupiditate non officiis consequatur ea cum accusamus sed. Reprehenderit corrupti vitae minima enim quo. Nihil fugit excepturi nemo praesentium.</p>
+
+                    <br />
+
+                    <p>Created with Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+
+                    <br />
+                    <br />
+
+                    <p>Link to site</p>
+                    <p>Link to repo</p>
+                </div>
+
+                <div className="project-preview"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
