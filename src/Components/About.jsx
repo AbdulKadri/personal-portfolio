5aca2935f5e26c5b3e2058c82bc1b0b796d1c0a4
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-03-23 19:18:41 -0600

    Add description of myself

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,15 @@ function About() {
         <div className="About" id="about">
             <h2> <u>About</u> </h2>
 
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio non voluptatem, aut dolorem quibusdam sint iusto minus dolores suscipit cum. Commodi debitis error molestias deleniti dolorem magnam consequuntur eum provident?</p>
+            <p>Hi there! My name is Abdul-Rahime Kadri and I'm a web developer with a passion for creating beautiful and functional websites.
+                <br />
+                <br />
+                Although I studied biological sciences in university, I discovered my love for coding during a computer science elective course. Since then, I've been honing my skills as a self-taught developer and have worked on a variety of projects, from simple landing pages to complex e-commerce sites.
+                <br />
+                <br />
+                Im a big fan of all kinds of sports so i always try to find a way to incoprate some type of sports related content into my projects.
+                If you're interested in collaborating, don't hesitate to get in touch. I'm always looking for new projects and collaborations. Thanks for checking out my portfolio!
+            </p>
 
             <EmblaCarousel />
         </div>
