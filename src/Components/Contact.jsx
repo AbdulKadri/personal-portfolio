ba70151232c65f676f6bb250517fe7fe53be731c
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-03-25 15:58:49 -0600

    Add functionality to email section

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,36 +1,74 @@
-import React from "react";
+import { useRef, useState } from 'react';
+import emailjs from 'emailjs-com'
 import Asset from "./Assets";
 import '../styles/Contact.scss'
 
 const Contact = () => {
+    const form = useRef()
+    const [inputs, setInputs] = useState({})
+
+    const handleChange = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('service_vllsmxj', 'template_2bb0yqg', form.current, 'fKBdg7i_NYWLk0db1')
+        setInputs({})
+    }
+
     return (
         <div className="contact" id="contact">
             <h2>Contact</h2>
 
             <div className="contact-container">
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <fieldset>
                         <legend>Send me an Email</legend>
                         <div className="name-container">
-                            <input type="text" id="userName" placeholder="Name" required />
+                            <input
+                                type="text"
+                                id="userName"
+                                placeholder="Name"
+                                name='name'
+                                value={inputs.name || ""}
+                                onChange={handleChange}
+                                required />
                         </div>
 
                         <div className="email-container">
-                            <input type="text" id="userEmail" placeholder="Email" required />
+                            <input
+                                type="email"
+                                id="userEmail"
+                                placeholder="Email"
+                                name='email'
+                                value={inputs.email || ""}
+                                onChange={handleChange}
+                                required />
                         </div>
 
                         <div className="text-container">
-                            <textarea name="userText" id="userText" rows="10" placeholder="Text" required></textarea>
+                            <textarea
+                                id="userText"
+                                rows="10"
+                                placeholder="Message"
+                                name="message"
+                                value={inputs.message || ""}
+                                onChange={handleChange}
+                                required>
+                            </textarea>
                         </div>
 
-                        <button>Send <img src={ Asset.Arrow } alt="arrow" /></button>
+                        <button type='submit'>Send <img src={Asset.Arrow} alt="arrow" /></button>
                     </fieldset>
                 </form>
             </div>
 
             <div className="social-container">
-                <a href="https://github.com/AbdulKadri" target="_blank" rel="noreferrer"><img src={ Asset.logoGithub } alt="Github logo" /></a>
-                <a href="https://linkedin.com/in/abdul-rahime-kadri-bb8935178" target="_blank" rel="noreferrer"><img src={ Asset.logoLinkedIn } alt="LinkedIn logo" /></a>
+                <a href="https://github.com/AbdulKadri" target="_blank" rel="noreferrer"><img src={Asset.logoGithub} alt="Github logo" /></a>
+                <a href="https://linkedin.com/in/abdul-rahime-kadri-bb8935178" target="_blank" rel="noreferrer"><img src={Asset.logoLinkedIn} alt="LinkedIn logo" /></a>
             </div>
         </div>
     )
