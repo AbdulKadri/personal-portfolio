199853ef3b40737c8aefb965feaa1e0bbd407ddd
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-05-18 14:56:00 -0600

    Add project descriptions and titles

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -7,21 +7,21 @@ const Projects = () => {
 
             <div className="project">
                 <div className="project-text">
-                    <h3>Project 1</h3>
+                    <h3>Sports Nexus (Social Media App)</h3>
                     <br />
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iste asperiores ut dolorum obcaecati vero ullam praesentium dolore corrupti nulla? Odio voluptates harum quasi in? Neque, quod? Aut, adipisci amet!</p>
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat earum nobis cupiditate non officiis consequatur ea cum accusamus sed. Reprehenderit corrupti vitae minima enim quo. Nihil fugit excepturi nemo praesentium.</p>
+                    <p className='project-description'>A full stack MERN project that allows you to make an account, create and delete posts, add friends and check out profiles! Includes a dark mode feature, toasts for succesful register/login, and is authenticated with JWT Auth. (Search Capability is still under work)</p>
 
                     <br />
 
-                    <p>Created with Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <p className='created-with'><u>Frontend:</u> React, Material UI, Redux Toolkit, React Router Dom, Formik, Yup, Dropzone...</p>
+                    <p className='created-with'><u>Backend:</u> Node/Express, MongoDB/Mongoose, JWT, Postman, Bcrypt, Multer, Helmet, Morgan...</p>
+                    <p className='created-with'><u>Hosting:</u> ...</p>
 
                     <br />
 
                     <a href="https://github.com/AbdulKadri" target="_blank" rel="noreferrer">Live site</a>
-                    <a href="https://github.com/AbdulKadri" target="_blank" rel="noreferrer"> Repository </a>
+                    <a href="https://github.com/AbdulKadri/social-media" target="_blank" rel="noreferrer"> Repository </a>
                 </div>
 
                 <div className="project-preview">
@@ -45,41 +45,40 @@ const Projects = () => {
                 </div>
 
                 <div className="project-text">
-                    <h3>Project 2</h3>
+                    <h3>Apex Weather (Weather App)</h3>
                     <br />
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iste asperiores ut dolorum obcaecati vero ullam praesentium dolore corrupti nulla? Odio voluptates harum quasi in? Neque, quod? Aut, adipisci amet!</p>
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat earum nobis cupiditate non officiis consequatur ea cum accusamus sed. Reprehenderit corrupti vitae minima enim quo. Nihil fugit excepturi nemo praesentium.</p>
+                    <p className='project-description'>A Next.js project that allows you to search for any city's weather, forecast and air quality index. Includes a heat weather map, image of cities, and ability to switch units for metric and imperial. Also has a 3D model of the globe with animation features.</p>
 
                     <br />
 
-                    <p>Created with Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <p className='created-with'><u>Frontend:</u> React, Next, Three.js, GSAP, TailwindCSS, react-toastify, axios...</p>
+                    <p className='created-with'><u>Hosting:</u> Vercel</p>
 
                     <br />
 
                     <a href="https://github.com/AbdulKadri" target="_blank" rel="noreferrer">Live site</a>
-                    <a href="https://github.com/AbdulKadri" target="_blank" rel="noreferrer"> Repository </a>
+                    <a href="https://github.com/AbdulKadri/next-weather-app" target="_blank" rel="noreferrer"> Repository </a>
                 </div>
             </div>
 
             <div className="project">
                 <div className="project-text">
-                    <h3>Project 3</h3>
+                    <h3>Yeg Dealership (Car dealership)</h3>
                     <br />
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iste asperiores ut dolorum obcaecati vero ullam praesentium dolore corrupti nulla? Odio voluptates harum quasi in? Neque, quod? Aut, adipisci amet!</p>
-
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat earum nobis cupiditate non officiis consequatur ea cum accusamus sed. Reprehenderit corrupti vitae minima enim quo. Nihil fugit excepturi nemo praesentium.</p>
+                    <p className='project-description'>A Wordpress project that includes a car inventory page to check out all the cars and details. also has testimonials, reviews, about section, contact section and more to simulate a real world car dealership website.</p>
 
                     <br />
 
-                    <p>Created with Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <p className='created-with'><u>Frontend:</u> Wordpress, Elementor and CSS.</p>
+                    <p className='created-with'><u>Hosting:</u> Hostinger</p>
+
+                    <br />
 
                     <br />
 
                     <a href="https://github.com/AbdulKadri" target="_blank" rel="noreferrer">Live site</a>
-                    <a href="https://github.com/AbdulKadri" target="_blank" rel="noreferrer"> Repository </a>
                 </div>
 
                 <div className="project-preview">
@@ -89,6 +88,34 @@ const Projects = () => {
                     <div className="project-back">
                         <p>Temp back</p>
                     </div>
+                </div>
+            </div>
+
+            <div className="project">
+                <div className="project-preview">
+                    <div className="project-front">
+                        <p>Temp Front</p>
+                    </div>
+                    <div className="project-back">
+                        <p>Temp back</p>
+                    </div>
+                </div>
+
+                <div className="project-text">
+                    <h3>ECommerce Site (Coming Soon...)</h3>
+                    <br />
+
+                    <p className='project-description'>A full stack project that gives you all the features of a real world eCommerece store. Includes Stripe API for payment and Auth0 for login and signup.</p>
+
+                    <br />
+
+                    <p className='created-with'><u>Frontend:</u> Vite, React, TypeScript...</p>
+                    <p className='created-with'><u>Hosting:</u> Vercel</p>
+
+                    <br />
+
+                    <a href="https://github.com/AbdulKadri" target="_blank" rel="noreferrer">Live site</a>
+                    <a href="https://github.com/AbdulKadri/next-weather-app" target="_blank" rel="noreferrer"> Repository </a>
                 </div>
             </div>
         </div>
