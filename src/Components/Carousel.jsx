0585e9a365eb036ad09951a612630abc48d5bc79
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-05-18 19:55:55 -0600

    Change from EmblaCarousel to Swiper js

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,36 +1,47 @@
-import { useCallback } from "react";
-import useEmblaCarousel from "embla-carousel-react";
-import Skills from "./Skills";
-import Autoplay from "embla-carousel-autoplay";
+import Skills from './Skills';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import '../styles/Carousel.scss'
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/autoplay';
+import 'swiper/scss/mousewheel'
+import 'swiper/scss/free-mode';
+import SwiperCore, { Navigation, Autoplay, Mousewheel, A11y } from 'swiper';
 
-const autoplayOptions = {
-  delay: 2000,
-  jump: true,
+SwiperCore.use([Navigation, Autoplay, Mousewheel, A11y]);
+
+const Carousel = () => {
+    return (
+        <Swiper
+            modules={[Navigation, Autoplay, Mousewheel, A11y]}
+            spaceBetween={10}
+            slidesPerView={3}
+            breakpoints={{
+                640: {
+                    slidesPerView: 5,
+                },
+                768: {
+                    slidesPerView: 6,
+                },
+            }}
+            navigation
+            mousewheel={true}
+            autoplay={{ delay: 2000, disableOnInteraction: false }}
+            freeMode={true}
+            loop={true}
+            className='swiper-container'
+        >
+            {Skills.map((skill, index) => (
+                <SwiperSlide key={index}>
+                    <div className="skill">
+                        <img src={skill.logo} alt={`${skill.name} logo`} />
+                        <p>{skill.name}</p>
+                        <p className="ordered-number">{index + 1}</p>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
 }
 
-const EmblaCarousel = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, dragFree: true, align: 0 }, [Autoplay(autoplayOptions, (emblaRoot) => emblaRoot.parentElement)])
-
-  const scrollPrev = useCallback(() => {
-    if (emblaApi) emblaApi.scrollPrev()
-  }, [emblaApi])
-
-  const scrollNext = useCallback(() => {
-    if (emblaApi) emblaApi.scrollNext()
-  }, [emblaApi])
-
-  return (
-    <div className="embla">
-      <button className="embla-Prev" onClick={scrollPrev}>&#8249;</button>
-
-      <div className="embla-wrapper" ref={emblaRef}>
-        <Skills />
-      </div>
-
-      <button className="embla-Next" onClick={scrollNext}>&#8250;</button>
-    </div>
-  )
-}
-
-export default EmblaCarousel
+export default Carousel
